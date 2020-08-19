aa28773c90fae9799479be3e8fe0de4afb7bcded
Louis Stephens <lstephens@chumneyads.com>
2020-08-19 17:44:47 -0400

    navigation added

--- partials/header.php
+++ partials/header.php
@@ -5,22 +5,46 @@
 <!-- contact -->
 
 <!-- header -->
-<div id="header" class="flex">
-    <div id="logo">
-        <a href="./"><img src="dist/images/logo.png" /></a>
-    </div>
+<div id="header">
 
-    <div id="nav">
-        <ul>
-            <li><a href="./entrance-modal.php">Entrance Modal</a></li>
-            <li><a href="./exit-modal.php">Exit Modal</a></li>
-            <li><a href="./scratch-off-modals">Scratch-off Modals</a></li>
-            <li><a href="./video-modals.php">Video Modals</a></li>
-            <li><a href="./hover-buttons.php">Hover Buttons</a></li>
-            <li><a href="./insertion-buttons.php">Insertion Buttons</a></li>
-            <li><a href="./database-email-campaigns.php">Database Email Campaigns</a></li>
-            <li><a href="./scratch-off.php">Trade Evaluation</a></li>
-        </ul>
+    <!-- container -->
+    <div class="container">
+        <div id="logo">
+            <a href="./"><img src="dist/images/logo.png" /></a>
+        </div>
+
+        <div id="nav">
+            <ul class="menu">
+                <li>
+                    <a href="#">Modals +</a>
+                        <ul>
+                            <li><a href="./services/modals/entrance-modal.php">Entrance Modal</a>
+                            <li><a href="./exit-modal.php">Exit Modal</a>
+                            <li><a href="./scratch-off-modal.php">Scratch-off Modal</a>
+                            <li><a href="./video-modal.php">Video Modal</a>
+                        </ul>    
+                </li>
+                <li>
+                    <a href="#">Buttons +</a>
+                        <ul>
+                            <li><a href="./services/buttons/insertion-buttons.php">Insertion Buttons</a>
+                            <li><a href="./services/buttons/hover-buttons.php">Hover Buttons</a>
+                            <li><a href="./services/buttons/program-specific-buttons.php">Program Specific Buttons</a>
+                        </ul>    
+                </li>
+                <li>
+                    <a href="">Email Campaigns +</a>
+                        <ul>
+                            <li><a href="./services/emails/database-email-campaigns.php">Database Email Campaigns</a>
+                            <li><a href="./services/emails/hover-buttons.php">Hover Buttons</a>
+                            <li><a href="./services/emails/program-specific-buttons.php">Program Specific Buttons</a>
+                        </ul>    
+                </li>
+                <li><a href="./services/modals/trade-evaluation.php">Trade Evaluation</a></li>
+            </ul>
+        </div>
     </div>
+    <!-- container -->
+
 </div>
     <!-- header -->
---
+++ vehicle.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ABC Motors Sales</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="dist/css/vehicles.css">
+</head>
+<body>
+
+
+
+<!-- app -->
+<div id="home">
+
+   <?php include("partials/header.php"); ?>
+
+
+    <!-- main -->
+    <div id="main">
+        
+        <!-- banner -->
+        <div id="banner">
+
+
+        </div>
+        <!-- banner -->
+
+
+
+        <!-- vehicle -->
+        <div id="vehicle">
+
+            <!-- left -->
+            <div id="left">
+                <div id="top">
+                    <div class="vehicle-image"></div>
+                    <div class="vehicle-details"></div>
+                </div>    
+            </div>    
+            <!-- left -->
+
+            <!-- right -->
+            <div id="right">
+            </div>    
+            <!-- right -->
+
+           
+        </div>
+        <!-- vehicle -->
+
+        
+        <!-- similar -->
+        <div id="similar">
+        
+        </div>
+        <!-- similar -->
+
+
+
+    </div>
+    <!-- main -->
+
+
+
+
+</div>
+<!-- app -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="dist/js/all-min.js"></script>
+<!--<script src="dist/js/chatus-min.js"></script>-->
+<!--<script src="dist/js/inventory-min.js"></script>-->
+
+
+<script type="text/javascript">
+
+
+</script
+
+
+
+
+</body>
+</html>
